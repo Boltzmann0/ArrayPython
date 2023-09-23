659ea980df811145fbab08bff71d0d7f1cb841da
Lucas Lopes <lucas.dalps@gmail.com>
2023-09-23 17:21:47 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# Array de produtos de uma loja de eletrônicos
+produtos_eletronicos = ["Smartphone", "TV 4K", "Fones de ouvido sem fio", "Notebook", "Câmera DSLR"]
+
+# Array de anos de nascimento de familiares e amigos
+anos_nascimento = [1990, 1985, 1992, 1988, 2000]
+
+# Imprimir os arrays no terminal
+print("Produtos Eletrônicos na Loja:")
+for produto in produtos_eletronicos:
+    print(produto)
+
+print("\nAnos de Nascimento de Familiares e Amigos:")
+for ano in anos_nascimento:
+    print(ano)
